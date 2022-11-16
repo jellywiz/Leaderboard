a8a80bf2ed00fcdc552b0dd2da35b76eb0164eff
jellywiz <muzaffer.dler@gmail.com>
2022-11-16 15:14:24 +0300

    Update App.js for api and dom manipulation

--- src/js/App.js
+++ src/js/App.js
@@ -1,2 +1,32 @@
 import '../style.css';
-import './person.js';
+import Person from './person.js';
+import API from './API.js';
+import { recentScores, refreshBtn, submitBtn } from './elements.js';
+
+const lodash = require('lodash');
+
+const api = new API();
+
+const render = async () => {
+  recentScores.innerHTML = '';
+  const response = await api.getData();
+  const persons = new Person(response);
+  persons.players = lodash.sortBy(persons.players, ['score']).reverse();
+  persons.players.forEach((person) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${person.user} : ${person.score}`;
+    recentScores.appendChild(li);
+  });
+};
+submitBtn.addEventListener('click', async () => {
+  const name = document.querySelector('.name-input').value;
+  const score = document.querySelector('.score-input').value;
+  await api.addData(name, score);
+  render();
+});
+
+refreshBtn.addEventListener('click', () => {
+  render();
+});
+
+window.onload = render();
